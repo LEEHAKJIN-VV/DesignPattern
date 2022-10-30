ae6a3e7ede7de65fed68003c02349097ac2c48ae
LEEHAKJIN-VV <52197436+LEEHAKJIN-VV@users.noreply.github.com>
2022-10-30 22:01:25 +0900

    refactor other pattern code

--- DesignPattern/행위패턴/전략패턴(Strategy Pattern)/SimulatorDuck.swift
+++ DesignPattern/행위패턴/전략패턴(Strategy Pattern)/SimulatorDuck.swift
@@ -5,14 +5,14 @@
 //  Created by 이학진 on 2022/10/16.
 //
 
-//import Foundation
-//
-//var mallard: Duck = MallardDuck()
-//
-//mallard.performQuack()
-//mallard.performFly()
-//
-//var model: Duck = ModelDuck()
-//model.performFly()
-//model.setFlyBehavior(fb: FlyRocketPowered())
-//model.performFly()
+import Foundation
+
+var mallard: Duck = MallardDuck()
+
+mallard.performQuack()
+mallard.performFly()
+
+var model: Duck = ModelDuck()
+model.performFly()
+model.setFlyBehavior(fb: FlyRocketPowered())
+model.performFly()

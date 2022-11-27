02e1072b7b949080b576daba6f629067f38f5a83
LEEHAKJIN-VV <52197436+LEEHAKJIN-VV@users.noreply.github.com>
2022-11-27 11:42:42 +0900

    Update: Singleton.swift

--- DesignPattern/생성패턴/싱글톤패턴(Singleton Pattern)/Singleton.swift
+++ DesignPattern/생성패턴/싱글톤패턴(Singleton Pattern)/Singleton.swift
@@ -12,3 +12,13 @@ public class Singleton {
     
     private init() {}
 }
+
+var shared1 = Singleton.uniqueInstance
+var shared2 = Singleton.uniqueInstance
+
+shared1.value = 1
+print("shared1.value: \(shared1.value), shared2.value: \(shared2.value)")
+// Prints shared1.value: 1, shared2.value: 1
+shared2.value = 2
+print("shared1.value: \(shared1.value), shared2.value: \(shared2.value)")
+// Prints shared1.value: 2, shared2.value: 2
